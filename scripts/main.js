6da4f522423fa7f1b9aa0f3730fac0cec1af7918
Walid Berrouk <jw_berrouk@esi.dz>
2022-04-14 02:55:33 +0100

    fixing map

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function( $ ) {
+
+  
+    // Initialize Venobox
+    $('.venobox').venobox({
+      bgcolor: '',
+      overlayColor: 'rgba(47, 49, 56, 0.85)',
+      closeBackground: '',
+      closeColor: '#fff'
+    });
+  
+
+  
+
+  
+
+  
+  
+  });
+  
